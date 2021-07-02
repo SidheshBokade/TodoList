2ce98876f6809194808a250aadd09501863a4971
SidheshBokade <86685872+SidheshBokade@users.noreply.github.com>
2021-07-02 19:01:19 +0530

    first commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            <p>This is the Todolist.
+            Helpfull to keep record of your daily scedule</p>
+        </div>
+    )
+}
